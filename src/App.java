2d32283c8edbd50883325ba92b0bb323ac2ea84e
teonasnetto <teonasnetto@gmail.com>
2022-09-18 20:53:13 -0300

    Created class Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position position = new Position(1, 5);
+        System.out.println(position);
     }
 }
