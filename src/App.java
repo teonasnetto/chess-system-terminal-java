29442f266f68190cee4e8736319fd7c54b23db7e
teonasnetto <teonasnetto@gmail.com>
2022-09-18 21:09:28 -0300

    Starting to implemente Boarda and Piece Class

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position position = new Position(1, 5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }
