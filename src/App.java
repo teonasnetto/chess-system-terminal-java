124fe354cf30952ba018439b441f3ee2e012ee7d
teonasnetto <teonasnetto@gmail.com>
2022-09-21 18:34:56 -0300

    Printing possible moves

--- src/App.java
+++ src/App.java
@@ -10,23 +10,26 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
-        ChessMatch chessmatch = new ChessMatch();
+        ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
 
             try {
                 
                 UI.clearScreen();
-                UI.printBoard(chessmatch.getPieces());
+                UI.printBoard(chessMatch.getPieces());
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition sourcePosition = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(sourcePosition);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition targetPosition = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessmatch.performChessMove(sourcePosition, targetPosition);
+                ChessPiece capturedPiece = chessMatch.performChessMove(sourcePosition, targetPosition);
 
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
